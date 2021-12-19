3e258d7f2c2cb64793541f41b0642c1f254665f6
Gaurav Huria <gauravhuria@gmail.com>
2021-12-19 00:51:07 +0000

    Updated controller

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -35,7 +35,14 @@ namespace dotnetcore_webapi.Controllers
             dependencyService1.Write();
             dependencyService2.Write();
             
-            return Enumerable.Empty<WeatherForecast>();
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
         }
     }
 }
